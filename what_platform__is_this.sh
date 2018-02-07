155a175aa2db514e59197f0d5e0363893015a549
Jeremy Donson <jjdonson@gmail.com>
2018-02-07 09:13:31 -0500

    Cleaning up readme and trying to write detect script.

---
+++ what_platform__is_this.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script Author:  Jeremy Donson, NYC; jjdonson@gmail.com
+
+# Script Purpose:  
+
+# Execution Pattern:  $ bash what_platform__is_this.sh
+
+# Potential Risks:  NONE, as this is a read-only script.
+
+echo 
